7f1a122889bcbaeb52a86d83f5a2a30a652a75d0
chivaakh <chivakhteira16@gmail.com>
2025-06-04 16:41:28 +0000

    p

--- ishrili/ishrili/settings.py
+++ ishrili/ishrili/settings.py
@@ -121,7 +121,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'stage',
         'USER': 'root',
-        'PASSWORD': '1234',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '3306',
     }
